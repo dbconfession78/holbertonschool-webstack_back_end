449a83dcca5bf631ab0fd2959954d2d0167e8455
Stuart <sgkur04@gmail.com>
2018-07-21 20:41:47 +0000

    delete school's test file

--- 0x00-restful_api_users/models/base_model.py
+++ 0x00-restful_api_users/models/base_model.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-
 """
-module: base_model
+Module: contains BaseModel class definitiaon
 """
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """
-    BaseModel - defines the parent model for all other model types
-    """
+    """ defines the parent model for all other model types """
+    id = None
+    created_at = None
+    updated_at = None
+
     def __init__(self):
         """ initializes BaseModel """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.updated_at = self.created_at
