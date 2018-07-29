80cd6fcf17f521345e15bd101487333dd8f166e9
Stuart <sgkur04@gmail.com>
2018-07-29 03:05:29 +0000

    set condition for when to create db_engine

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -7,11 +7,15 @@ user = os.getenv("HBNB_YELP_MYSQL_USER")
 pw = os.getenv("HBNB_YELP_MYSQL_PWD")
 host = os.getenv("HBNB_YELP_MYSQL_HOST")
 db = os.getenv("HBNB_YELP_MYSQL_DB")
-db_engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(user, pw,
-                                                               host, db))
+db_engine = None
 
+if db:
+    db_engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(user, pw,
+                                                                   host, db))
 if os.getenv("HBNB_YELP_ENV") == "test":
     Base.metadata.drop_all(db_engine)
-Base.metadata.create_all(db_engine)
-db_session = scoped_session(sessionmaker(bind=db_engine,
-                                         expire_on_commit=False))
+
+if db_engine:
+    Base.metadata.create_all(db_engine)
+    db_session = scoped_session(sessionmaker(bind=db_engine,
+                                             expire_on_commit=False))
