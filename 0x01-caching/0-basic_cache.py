4cd04c32d1a92cd0a96c48ac092ca910147b6f14
Stuart <sgkur04@gmail.com>
2018-08-05 04:19:03 +0000

    0-basic_cache.py :initial commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+0-basic_cache - contains class definition for BasicCache
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Class definition for 'BasicCache'"""
+    def put(self, key, item):
+        """Stores a key/value pair to the parent 'cache_data' dictionary"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns a key's value from parent dictionary, 'cache_data'"""
+
+        return self.cache_data.get(key)
