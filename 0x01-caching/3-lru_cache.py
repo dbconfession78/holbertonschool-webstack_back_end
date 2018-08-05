c2bf8b11d0b1e5e13208f357ece8e46788f62d31
Stuart <sgkur04@gmail.com>
2018-08-05 22:56:23 +0000

    3-lru_cache.py :initial commit

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+3-lru_cache.py - contains class definition for LRUCache
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    : Class definition for 'LRUCache'
+    : @dct1: key to access-index dictionary
+    : @dct2: access-index to key dictionary
+    """
+    def __init__(self):
+        super().__init__()
+        self.dct1 = {}
+        self.dct2 = {}
+        self.idx = 0
+
+    def put(self, key, item):
+        """Stores a key/value pair to the parent 'cache_data' dictionary"""
+        if key and item:
+            self.cache_data[key] = item
+            self.access(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                key = min(list(self.dct2.keys()))
+                to_remove = self.dct2.get(key)
+                print("DISCARD: {}".format(to_remove))
+                del self.cache_data[to_remove]
+                del self.dct1[to_remove]
+                del self.dct2[key]
+
+    def get(self, key):
+        """Returns a key's value from parent dictionary, 'cache_data'"""
+        val = self.cache_data.get(key)
+        if val:
+            self.access(key)
+        return val
+
+    def access(self, key):
+        """ Updates key's access order in two dictionaries """
+        curr_idx = self.dct1.pop(key) if self.dct1.get(key) else None
+        if curr_idx:
+            del self.dct2[curr_idx]
+        self.dct1[key] = self.idx
+        self.dct2[self.idx] = key
+        self.idx += 1
