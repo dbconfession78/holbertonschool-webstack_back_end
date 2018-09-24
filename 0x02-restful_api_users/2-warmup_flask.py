c6393d37b16274335a9c681e2cac13b68fd58c6e
Stuart Kuredjian <sgkur04@gmail.com>
2018-09-24 00:26:15 +0000

    use env vars for api host and port

--- 0x02-restful_api_users/2-warmup_flask.py
+++ 0x02-restful_api_users/2-warmup_flask.py
@@ -3,23 +3,22 @@
 2-warmup_flask: start's a Flask application
 """
 from flask import Flask
+from os import getenv
 
 app = Flask(__name__)
-host = '0.0.0.0'
-port = 5050
+host = getenv('HBNB_API_HOST')
+port = getenv('HBNB_API_PORT')
 
 
 @app.route('/', strict_slashes=False)
 def get_hbs_string():
     """ Returns 'Holberton School' when the route is called """
-    app.url_map.strict_slashes = False
     return "Holberton School"
 
 
 @app.route('/c', strict_slashes=False)
 def get_c_string():
     """ Return's 'C is fun!' when the route is called """
-    app.url_map.strict_slashes = False
     return "C is fun!"
 
 
