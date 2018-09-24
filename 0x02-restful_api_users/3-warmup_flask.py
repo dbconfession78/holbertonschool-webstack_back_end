6976750c1657a2b92d892ea56cd18af0b394e554
Stuart Kuredjian <sgkur04@gmail.com>
2018-09-24 00:42:52 +0000

    doc and strict slash changes

--- 0x02-restful_api_users/3-warmup_flask.py
+++ 0x02-restful_api_users/3-warmup_flask.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+"""
+3-warmup_flask: starts a Flask app
+"""
 from flask import (Flask, jsonify)
 from os import getenv
 
 app = Flask(__name__)
 host = getenv('HBNB_API_HOST')
 port = getenv('HBNB_API_PORT')
-app.url_map.strict_slashes = False
 
 
 @app.route('/hbtn')
 def get_json():
+    """ Returns jsonified response when route is called """
+    app.url_map.strict_slashes = False
     dictionary = {
-        "C": "is_fun",
+        "C": "is fun",
         "Python": "is cool",
         "Sysadmin": "is hiring"
     }
