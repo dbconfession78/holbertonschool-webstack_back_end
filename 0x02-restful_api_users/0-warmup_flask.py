124003236a018affc985e6893be8f22e9e4ab329
Stuart Kuredjian <sgkur04@gmail.com>
2018-09-24 00:08:16 +0000

    move strict slashes rule inside method instead of in route decorator. Also put ip and port directly in app.run call instead of as global variables

--- 0x02-restful_api_users/0-warmup_flask.py
+++ 0x02-restful_api_users/0-warmup_flask.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""
+0-warmup_flask: starts an app
+"""
 from flask import Flask
 
 app = Flask(__name__)
-host = '0.0.0.0'
-port = 5000
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def fetch():
+    """ Returns 'Holberton School' when the '/' route is called """
+    app.url_map.strict_slashes = False
     return "Holberton School"
 
 if __name__ == "__main__":
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=5000)
